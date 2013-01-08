41b6a43c0e54322056a5f1c8d4d9ef3fdf4a096d
Tino Ehrich <ehrich@efides.com>
2013-01-08 12:18:05 +0100

    init

---
+++ test/RemoteServerMock/index.php
@@ -0,0 +1,5 @@
+<?php
+
+  $json = file_get_contents('php://input');
+  $requestArray = json_decode($json, TRUE);
+  echo json_encode(['id' => $requestArray['id'], 'result' => 'Hello World']);
---
+++ test/test.php
@@ -0,0 +1,12 @@
+<?php
+
+  require __DIR__ . '/../vendor/autoload.php';
+
+  $response = (new JsonRpcCurl())
+    ->setUrl('http://localhost/opensource/server/jsonrpc_curl/test/RemoteServerMock')
+    ->setId(1)
+    ->setMethod('Foo.bar.fooBarMethod')
+    ->setData(['name' => 'Mr. X'])
+    ->send();
+
+  var_dump($response);
